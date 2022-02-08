7c3ea3b136a6689beb614ff909c50919cae74649
erlanggadwi <64958442+erlanggadwi@users.noreply.github.com>
2022-02-08 13:25:54 +0700

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -34,7 +34,7 @@ do
 
   for i in 1 2 3 4; 
   do
-    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url -H "User-Agent: Caliph/UptimeRobot; (https://stats.justcaliph.tech)")
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
     if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
       result="success"
     else
